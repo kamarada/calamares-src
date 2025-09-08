f9a82a9ec201f3fa0a31f195e6fd566201f34426
Antônio Medeiros <contato@antoniomedeiros.dev>
2025-09-08 20:31:50 -0300

    kamarada/features#63 - installation slideshow

---
+++ src/branding/kamarada/slideshow/show_pt_BR.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pt_BR">
+<context>
+    <name>show</name>
+    <message>
+        <location filename="show.qml" line="43"/>
+        <source>&lt;strong&gt;Installing Linux Kamarada&lt;/strong&gt;&lt;br/&gt;&lt;br/&gt;Copying the system files. This might take a few minutes.</source>
+        <translation>&lt;strong&gt;Instalando o Linux Kamarada&lt;/strong&gt;&lt;br/&gt;&lt;br/&gt;Copiando os arquivos do sistema. Isso pode demorar alguns minutos.</translation>
+    </message>
+</context>
+</TS>
